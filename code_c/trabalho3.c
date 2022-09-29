12d15d1948093edb397d5f16b5007b0848795e4c
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-09-29 10:33:25 +0100

    Create trabalho3.c

---
+++ code_c/trabalho3.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+
+                            Online C Compiler.
+                
+Code, Compile, Run and Debug C program online.
+
+Write your code in this editor and press "Run" button to compile and execute it.
+
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+
+int main()
+
+{
+
+int x;
+ 
+printf("Escreva um numero inteiro:");
+ 
+scanf("%d",&x);
+ 
+printf("\no numero que escreveu foi: %d",x);
+ 
+printf("\no numero que escreveu foi: %c",x);
+    
+    
+return 0;
+
+} 
