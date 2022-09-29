471a26353410c210d21247ffc8d681d1063b18ee
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-09-29 09:59:51 +0100

    Create trabalho2.c

---
+++ code_c/trabalho2.c
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+
+                            Online C Compiler.
+               
+ Code, Compile, Run and Debug C program online.
+
+Write your code in this editor and press "Run" button to compile and execute it.
+
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+
+int main()
+
+{
+    
+int x1=71;
+    
+int x2=111;
+    
+int x3=110;
+    
+int x4=99;
+    
+int x5=97;
+    
+int x6=108;
+    
+int x7=111;
+    
+char z1='p';
+    
+    
+
+printf("%c%c%c%c%c%c%c\n" ,x1,x2,x3,x4,x5,x6,x7);
+    
+printf("%c%d%c%d%c%c%c\n" ,x1,x2,x3,x4,x5,x6,x7);
+    
+printf("%c%d\n",z1,z1);
+    
+    
+return 0;
+
+}
