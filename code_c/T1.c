da551ca700d5d6717bcea6aa1850824acc0d7583
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-10-06 10:22:38 +0100

    primeiro trabalho

---
+++ code_c/T1.c
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+ /*
+ incluir livrarias de entrada
+ e saída de dados
+ */
+#include <stdio.h>
+
+//função onde é feita a execução do programa
+int main()
+{
+    // código com identação
+    // identação - dentro de cada bloco de código haver alinhamento vertical
+    // imprimir na consola "Hello world"
+    printf("Hello World");
+    //impressão de uma linha, mudança de linha
+    printf("\n");
+    
+    printf("Gonçalo Pereira");
+    
+    printf("\ncoca-cola\nfcporto\n\nsemba-semba");
+    // valor de retorno da função main
+    
+    
+    
+    
+    int myNum =15;
+    printf("\n%d",myNum);
+    
+    int x1=30;
+    float x2=79.30;
+    char x3='G';
+    
+    printf("\n\n %d últimos dois números do CC", x1);
+    printf("\n\n últimos %d dois números do CC", x1);
+    printf("\n\n últimos dois números do CC %d", x1);
+
+    
+    
+    return 0;
+}
