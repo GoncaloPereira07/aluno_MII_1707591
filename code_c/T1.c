8953abaf904d6c660c5b6cb7df38e4b512f786da
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-10-12 10:46:16 +0100

    update código

--- code_c/T1.c
+++ code_c/T1.c
@@ -1,46 +1,44 @@
-/******************************************************************************
+/*
+ * *****************************************************************************
 
                             Online C Compiler.
                 Code, Compile, Run and Debug C program online.
 Write your code in this editor and press "Run" button to compile and execute it.
 
-*******************************************************************************/
- /*
- incluir livrarias de entrada
- e saída de dados
- */
+******************************************************************************
+*/
+/*
+ incluir livrarias de entra 
+ e saida de dadois
+*/
+
 #include <stdio.h>
 
-//função onde é feita a execução do programa
+// função onde é feita a execução do programa
 int main()
 {
-    // código com identação
-    // identação - dentro de cada bloco de código haver alinhamento vertical
-    // imprimir na consola "Hello world"
+    // código com Identação 
+    // Identação - dentro de cada bloco de código haver alinhamento vertical
+    // imprimir na consola "Hello world";
     printf("Hello World");
-    //impressão de uma linha, mudança de linha
+    // impressoa de uma linha, mudança linha
     printf("\n");
     
-    printf("Gonçalo Pereira");
+    printf("Paulo Alexandre");
     
-    printf("\ncoca-cola\nfcporto\n\nsemba-semba");
+    printf("\ncoica-cola\nfcporto\n\n semba-samba");
     // valor de retorno da função main
     
-    
-    
-    
-    int myNum =15;
+    int myNum=15;
     printf("\n%d",myNum);
     
-    int x1=30;
-    float x2=79.30;
-    char x3='G';
+    int x1=64;
+    float x2=21.23;
+    char x3='P';
     
-    printf("\n\n %d últimos dois números do CC", x1);
-    printf("\n\n últimos %d dois números do CC", x1);
-    printf("\n\n últimos dois números do CC %d", x1);
-
+    printf("\n %d ultimo dois numeros do CC", x1);
     
     
     return 0;
 }
+
