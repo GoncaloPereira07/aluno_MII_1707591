327ec0e133a8935dfb1f5a82a4f085b0a6f99b3e
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-10-20 10:41:09 +0100

    T16.c

---
+++ code_c/T16.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+
+                            Online C Compiler.
+
+                Code, Compile, Run and Debug C program online.
+
+Write your code in this editor and press "Run" button to compile and execute it.
+
+
+*******************************************************************************/
+
+
+#include <stdio.h>
+
+int main(){
+
+ for(int i=1;i<100;i++){
+
+ if(i%3==0) printf("Ok, ");
+
+ printf("o valor do inteiro gerado é: %d \n ",i);
+
+ }
+
+ return 0;
+
+}
