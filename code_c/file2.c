a5d4065e518d23488456e73148a6eea12a5efb77
GoncaloPereira07 <114104897+GoncaloPereira07@users.noreply.github.com>
2022-11-30 12:12:51 +0000

    .

---
+++ code_c/file2.c
@@ -0,0 +1,10 @@
+﻿// executado em ambiente windows
+#include <stdio.h>
+int main() {
+ FILE *fp;
+ fp = fopen("./program2.txt", "w+");
+ fprintf(fp, "Gonçalo Pereira ...\n");
+ // fputs("This is testing for fputs...\n", fp);
+ fclose(fp);
+ return 0;
+}
